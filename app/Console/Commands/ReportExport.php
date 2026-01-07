0c2a914f10f72652a43e002a42bbc3bb27d9797d
istrategy <gertvaneeden@gmail.com>
2026-01-07 19:29:41 +0200

    Add timestamp to report export.

--- app/Console/Commands/ReportExport.php
+++ app/Console/Commands/ReportExport.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Exports\TasksReportExport;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Storage;
 use Maatwebsite\Excel\Facades\Excel;
@@ -16,7 +17,7 @@ class ReportExport extends Command
     public function handle(): int
     {
         $directory = 'reports';
-        $filename = 'tasks_report.xlsx';
+        $filename = 'tasks_report' . '-' . Carbon::now()->format('Y-m-d_H-i-s') . '.xlsx';
         $path = $directory . '/' . $filename;
 
         if (!Storage::exists($directory)) {
